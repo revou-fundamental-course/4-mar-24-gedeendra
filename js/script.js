e28ae808b225ea627f1def82cf1d69092865ad29
gedeendra <gede.endrawadi@gmail.com>
2024-03-14 21:25:57 +0800

    editing style for input (focus and hover)

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function bmiCalculation()  {
     let bmiWeight = document.getElementById("bmiWeight").value;
     let bmiHeight = document.getElementById("bmiHeight").value;
     var areaResult = document.getElementById("area-result");
+    var extraCompliment = document.getElementById("category-two-a");
     var diseasesList = document.getElementById("diseases-list")
 
     if ((bmiRadio == "male" || bmiRadio == "female") && bmiAge != "" && bmiWeight != "" && bmiHeight != "") {
@@ -21,6 +22,7 @@ function bmiCalculation()  {
         if (bmiResult < 18.5) {
             document.getElementById("category-one").innerHTML = "Underweight";
             document.getElementById("category-two").innerHTML = "You are underweight.";
+            extraCompliment.style.display = "none";
             document.getElementById("category-three").innerHTML = "BMI result < 18.5";
             document.getElementById("category-four").innerHTML = "You are in the underweight category.";
             document.getElementById("category-five").innerHTML = "Contact a doctor for advice on proper nutrition and diet to improve health.";
@@ -32,7 +34,9 @@ function bmiCalculation()  {
             document.getElementById("category-ten").innerHTML = "Weak Immune System";
         } else if (bmiResult >= 18.5 && bmiResult < 25) {
             document.getElementById("category-one").innerHTML = "Normal";
-            document.getElementById("category-two").innerHTML = "You have an ideal weight. Good job!!";
+            document.getElementById("category-two").innerHTML = "You have an ideal weight.";
+            extraCompliment.style.display = "block";
+            document.getElementById("category-two-a").innerHTML = "Good job!!";
             document.getElementById("category-three").innerHTML = "BMI result between 18.5 and 25";
             document.getElementById("category-four").innerHTML = "You are in the normal weight category.";
             document.getElementById("category-five").innerHTML = "Keep maintaining your weight and balance your diet and physical activity.";
@@ -40,6 +44,7 @@ function bmiCalculation()  {
         } else if (bmiResult >= 25 && bmiResult < 30) {
             document.getElementById("category-one").innerHTML = "Overweight";
             document.getElementById("category-two").innerHTML = "You have an overweight.";
+            extraCompliment.style.display = "none";
             document.getElementById("category-three").innerHTML = "BMI result between 25 and 30";
             document.getElementById("category-four").innerHTML = "You are in the overweight category.";
             document.getElementById("category-five").innerHTML = "The best way to lose weight is by regulating calorie intake and exercising. If your BMI falls into this category, it is recommended to reduce weight to normal levels.";
@@ -52,6 +57,7 @@ function bmiCalculation()  {
         } else {
             document.getElementById("category-one").innerHTML = "Obesity";
             document.getElementById("category-two").innerHTML = "You are in the obesity category.";
+            extraCompliment.style.display = "none";
             document.getElementById("category-three").innerHTML = "BMI result is over 30.";
             document.getElementById("category-four").innerHTML = "You are in the obesity category.";
             document.getElementById("category-five").innerHTML = "Strive to lose weight and adopt a healthy lifestyle by watching your diet and engaging in physical activity. Visit a doctor soon for further health checks to assess the risks associated with your weight.";
