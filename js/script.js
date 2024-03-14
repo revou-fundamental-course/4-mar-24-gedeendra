c787a7af5517206b17580716ea0cf8028aa00239
gedeendra <gede.endrawadi@gmail.com>
2024-03-14 19:05:14 +0800

    editing style display action for button and make responsive page by using mobile simulator, chrome extension, and it's worked :)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-function bmiCalculation ()  {
+function bmiCalculation()  {
     // variable
-    let bmiMan = document.getElementById("bmiMan").checked;
-    let bmiWoman = document.getElementById("bmiWoman").checked;
+    const bmiRadio = document.querySelector("input[name=sex]:checked").value;
     let bmiAge = document.getElementById("bmiAge").value;
     let bmiWeight = document.getElementById("bmiWeight").value;
     let bmiHeight = document.getElementById("bmiHeight").value;
+    var areaResult = document.getElementById("area-result");
+    var diseasesList = document.getElementById("diseases-list")
 
-    if ((bmiMan == true || bmiWoman == true) && bmiAge != "" && bmiWeight != "" && bmiHeight != "") {
+    if ((bmiRadio == "male" || bmiRadio == "female") && bmiAge != "" && bmiWeight != "" && bmiHeight != "") {
+        // show the result
+        areaResult.style.display = "block";
+        
         // calculate BMI
         bmiResult = bmiWeight / ((bmiHeight / 100)*(bmiHeight / 100))
         
@@ -20,6 +24,7 @@ function bmiCalculation ()  {
             document.getElementById("category-three").innerHTML = "BMI result < 18.5";
             document.getElementById("category-four").innerHTML = "You are in the underweight category.";
             document.getElementById("category-five").innerHTML = "Contact a doctor for advice on proper nutrition and diet to improve health.";
+            diseasesList.style.display = "block";
             document.getElementById("category-six").innerHTML = "Low weight can lead to various health problems:";
             document.getElementById("category-seven").innerHTML = "Infertility";
             document.getElementById("category-eight").innerHTML = "Anemia";
@@ -31,12 +36,14 @@ function bmiCalculation ()  {
             document.getElementById("category-three").innerHTML = "BMI result between 18.5 and 25";
             document.getElementById("category-four").innerHTML = "You are in the normal weight category.";
             document.getElementById("category-five").innerHTML = "Keep maintaining your weight and balance your diet and physical activity.";
+            diseasesList.style.display = "none";
         } else if (bmiResult >= 25 && bmiResult < 30) {
             document.getElementById("category-one").innerHTML = "Overweight";
             document.getElementById("category-two").innerHTML = "You have an overweight.";
             document.getElementById("category-three").innerHTML = "BMI result between 25 and 30";
             document.getElementById("category-four").innerHTML = "You are in the overweight category.";
             document.getElementById("category-five").innerHTML = "The best way to lose weight is by regulating calorie intake and exercising. If your BMI falls into this category, it is recommended to reduce weight to normal levels.";
+            diseasesList.style.display = "block";
             document.getElementById("category-six").innerHTML = "Some diseases related to obesity:";
             document.getElementById("category-seven").innerHTML = "Diabetes";
             document.getElementById("category-eight").innerHTML = "Hypertension";
@@ -48,26 +55,25 @@ function bmiCalculation ()  {
             document.getElementById("category-three").innerHTML = "BMI result is over 30.";
             document.getElementById("category-four").innerHTML = "You are in the obesity category.";
             document.getElementById("category-five").innerHTML = "Strive to lose weight and adopt a healthy lifestyle by watching your diet and engaging in physical activity. Visit a doctor soon for further health checks to assess the risks associated with your weight.";
+            diseasesList.style.display = "block";
             document.getElementById("category-six").innerHTML = "Some diseases related to obesity:";
             document.getElementById("category-seven").innerHTML = "Diabetes";
             document.getElementById("category-eight").innerHTML = "Hypertension";
             document.getElementById("category-nine").innerHTML = "Heart Disease";
             document.getElementById("category-ten").innerHTML = "Osteoarthritis";
         }
-
     }
-
-    
 }
 
-function bmiReset () {
+function bmiReset() {
     let btnClear = document.querySelector("#button-reset");
     let inputNumber = document.querySelectorAll("input[type='tel']");
     let inputRadio = document.querySelectorAll("input[type='radio']");
-    
+    var areaResult = document.getElementById("area-result");
 
     btnClear.addEventListener("click",() => {
         inputNumber.forEach(input => input.value = "");
         inputRadio.forEach(input => input.checked = false);
+        areaResult.style.display = "none";
     })
 }
